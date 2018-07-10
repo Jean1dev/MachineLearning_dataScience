846d71779667704ec863c6cf3bd95a46e86d1290
jean <jeanlucafp@gmail.com>
2018-07-10 20:42:15 -0300

    treinametnos com o bd

--- scripts/pre_processamento_census.py
+++ scripts/pre_processamento_census.py
@@ -29,3 +29,8 @@ previsores = onehotencoder.fit_transform(previsores).toarray()
 
 labelencoder_classe = LabelEncoder()
 classe = labelencoder_classe.fit_transform(classe)
+
+#escalonamento
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
